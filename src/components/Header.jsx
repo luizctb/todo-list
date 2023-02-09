6adad48d24888ce3c487b5086e2bfd2ce59c1506
luizctb <luizcarlostb@hotmail.com>
2023-02-08 23:05:02 -0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import styles from '/Header.module.css';
+
+import logo from './assets/logo.svg';
+
+import { Header } from './Header';
+
+export function Header() {
+
+  return (
+    <article className={styles.headerContainer}>
+      <strong className={styles.header}>
+        <img src={logo} alt="Logotipo ToDo" />
+        <p>to<span>do</span></p>
+      </strong>
+
+    </article>
+  )
+};
+
